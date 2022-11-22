b79cc2c331ddee4c26038aa6cd2276f1a0d061e4
Daniel Kenney <danielkenney99@yahoo.com>
2022-11-21 19:10:40 -0800

    added the filter and dynamic adding

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,32 +1,35 @@
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
+import ExpensesFilter from "../ExpenseFilter/ExpensesFilter";
 
-const props = (props) => {
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2022");
+
+  const addFilterYear = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.lineItems[0].title}
-        amount={props.lineItems[0].amount}
-        date={props.lineItems[0].date}
-      />
-      <ExpenseItem
-        title={props.lineItems[1].title}
-        amount={props.lineItems[1].amount}
-        date={props.lineItems[1].date}
-      />
-      <ExpenseItem
-        title={props.lineItems[2].title}
-        amount={props.lineItems[2].amount}
-        date={props.lineItems[2].date}
-      />
-      <ExpenseItem
-        title={props.lineItems[3].title}
-        amount={props.lineItems[3].amount}
-        date={props.lineItems[3].date}
-      />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onFilterYear={addFilterYear} />
+        {props.lineItems
+          .filter((item) => {
+            return item.date.getFullYear() == filteredYear;
+          })
+          .map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))}
+      </Card>
+    </div>
   );
 };
 
-export default props;
+export default Expenses;
--- 01-starting-setup/src/components/NewExpense/NewExpense.js
+++ 01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -2,10 +2,18 @@ import React from "react";
 import "./NewExpense.css";
 import Expenseform from "./ExpenseForm";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+  };
+
   return (
     <div className="new-expense">
-      <Expenseform />
+      <Expenseform onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 };
